c9cdf3c7492f8ec1f718aa0a156b86833748f6be
berru <berruhanedar@icloud.com>
2024-12-26 03:31:21 +0300

    Refactored code to reduce repetition by introducing a playSound method and streamlined sound management

--- src/MusicManager.java
+++ src/MusicManager.java
@@ -1,82 +1,65 @@
 import javax.sound.sampled.*;
 import java.io.File;
-import java.io.IOException;public class MusicManager {
+
+public class MusicManager {
     private static Clip currentClip;
     private static Clip barkClip;
     private static Clip meowClip;
     private static Clip gameoverClip;
 
-    // Müzik çalmayı başlatma
     public static void playMusic(String musicFilePath) {
         try {
-            // Önceki müzik varsa durdur
             if (currentClip != null && currentClip.isRunning()) {
                 currentClip.stop();
             }
-
-            // Yeni müziği çalmaya başla
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(musicFilePath));
             currentClip = AudioSystem.getClip();
             currentClip.open(audioStream);
-            currentClip.loop(Clip.LOOP_CONTINUOUSLY); // Müzik sürekli döngüde çalsın
+            currentClip.loop(Clip.LOOP_CONTINUOUSLY);
             currentClip.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // Bark sesini çalma
-    public static void playBark() {
-        try {
-            if (barkClip != null && barkClip.isRunning()) {
-                barkClip.stop();
-            }
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File("resources/bark.wav"));
-            barkClip = AudioSystem.getClip();
-            barkClip.open(audioStream);
-            barkClip.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Meow sesini çalma
-    public static void playMeow() {
-        try {
-            if (meowClip != null && meowClip.isRunning()) {
-                meowClip.stop();
-            }
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File("resources/meow.wav"));
-            meowClip = AudioSystem.getClip();
-            meowClip.open(audioStream);
-            meowClip.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // GameOver sesini çalma
-    public static void playGameOver() {
-        try {
-            if (gameoverClip != null && gameoverClip.isRunning()) {
-                gameoverClip.stop();
-            }
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File("resources/gameover.wav"));
-            gameoverClip = AudioSystem.getClip();
-            gameoverClip.open(audioStream);
-            gameoverClip.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Müzik durdurma
     public static void stopMusic() {
         try {
             if (currentClip != null && currentClip.isRunning()) {
                 currentClip.stop();
             }
-            // Diğer sesleri de durdur
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void playBark() {
+        playSound("resources/bark.wav", barkClip);
+    }
+
+    public static void playMeow() {
+        playSound("resources/meow.wav", meowClip);
+    }
+
+    public static void playGameOver() {
+        playSound("resources/gameover.wav", gameoverClip);
+    }
+
+    private static void playSound(String soundFilePath, Clip soundClip) {
+        try {
+            if (soundClip != null && soundClip.isRunning()) {
+                soundClip.stop();
+            }
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(soundFilePath));
+            soundClip = AudioSystem.getClip();
+            soundClip.open(audioStream);
+            soundClip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void stopAllSounds() {
+        try {
             if (barkClip != null && barkClip.isRunning()) {
                 barkClip.stop();
             }
