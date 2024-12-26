633e92b49b2351dfd12f10feb0dc807ef53b9254
berru <berruhanedar@icloud.com>
2024-12-26 03:24:48 +0300

    Add resources

--- src/Cat.java
+++
@@ -1,9 +0,0 @@
-public class Cat extends Animal {
-    public Cat(String name) {
-        super(name);
-    }
-
-    public void meow() {
-        System.out.println(name + " is meowing!");
-    }
-}
--- src/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog extends Animal {
-    public Dog(String name) {
-        super(name);
-    }
-
-    public void bark() {
-        System.out.println(name + " is barking!");
-    }
-}
