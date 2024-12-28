ab9d84699f76cff4c23a186baa0e402818e1aae5
berru <berruhanedar@icloud.com>
2024-12-28 22:37:03 +0300

    Refactored MusicManager class to add additional sound clips and improve music stopping functionality

--- src/MusicManager.java
+++ src/MusicManager.java
@@ -6,18 +6,30 @@ public class MusicManager {
     private static Clip barkClip;
     private static Clip meowClip;
     private static Clip gameoverClip;
+    private static Clip catdogfightClip;
+
+    public static void playGameMusic() {
+        playMusic("resources/gameMusic.wav");
+    }
+
+    public static void playDefaultMusic() {
+        playMusic("resources/music.wav");
+    }
 
     public static void playMusic(String musicFilePath) {
         try {
-            if (currentClip != null && currentClip.isRunning()) {
-                currentClip.stop();
+            // Stop the previous music if needed
+            if (currentClip != null) {
+                stopMusic();
             }
+
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(musicFilePath));
             currentClip = AudioSystem.getClip();
             currentClip.open(audioStream);
-            currentClip.loop(Clip.LOOP_CONTINUOUSLY);
+            currentClip.loop(Clip.LOOP_CONTINUOUSLY); // Loop indefinitely
             currentClip.start();
         } catch (Exception e) {
+            System.err.println("Error playing music: " + musicFilePath);
             e.printStackTrace();
         }
     }
@@ -26,35 +38,48 @@ public class MusicManager {
         try {
             if (currentClip != null && currentClip.isRunning()) {
                 currentClip.stop();
+                currentClip.close();
+                currentClip = null; // Reset the clip
             }
         } catch (Exception e) {
+            System.err.println("Error stopping music.");
             e.printStackTrace();
         }
     }
 
+
     public static void playBark() {
-        playSound("resources/bark.wav", barkClip);
+        barkClip = playSound("resources/bark.wav", barkClip);
     }
 
     public static void playMeow() {
-        playSound("resources/meow.wav", meowClip);
+        meowClip = playSound("resources/meow.wav", meowClip);
     }
 
     public static void playGameOver() {
-        playSound("resources/gameover.wav", gameoverClip);
+        gameoverClip = playSound("resources/gameover.wav", gameoverClip);
     }
 
-    private static void playSound(String soundFilePath, Clip soundClip) {
+    public static void playCatDogFight() {
+        catdogfightClip = playSound("resources/catdogfight.wav", catdogfightClip);
+    }
+
+    private static Clip playSound(String soundFilePath, Clip existingClip) {
         try {
-            if (soundClip != null && soundClip.isRunning()) {
-                soundClip.stop();
+            if (existingClip != null && existingClip.isRunning()) {
+                existingClip.stop();
+                existingClip.close();
             }
+
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(soundFilePath));
-            soundClip = AudioSystem.getClip();
-            soundClip.open(audioStream);
-            soundClip.start();
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            return clip;
         } catch (Exception e) {
+            System.err.println("Error playing sound: " + soundFilePath);
             e.printStackTrace();
+            return null;
         }
     }
 
@@ -62,14 +87,26 @@ public class MusicManager {
         try {
             if (barkClip != null && barkClip.isRunning()) {
                 barkClip.stop();
+                barkClip.close();
             }
+
             if (meowClip != null && meowClip.isRunning()) {
                 meowClip.stop();
+                meowClip.close();
             }
+
             if (gameoverClip != null && gameoverClip.isRunning()) {
                 gameoverClip.stop();
+                gameoverClip.close();
             }
+
+            if (catdogfightClip != null && catdogfightClip.isRunning()) {
+                catdogfightClip.stop();
+                catdogfightClip.close();
+            }
+
         } catch (Exception e) {
+            System.err.println("Error stopping all sounds.");
             e.printStackTrace();
         }
     }
