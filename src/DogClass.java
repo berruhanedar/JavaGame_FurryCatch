1ae228399202d1c9484e039386da8cd093163ed9
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2025-01-07 18:52:59 +0300

    Refactored DogClass to extend AnimalClass and used constructor inheritance.

--- src/DogClass.java
+++ src/DogClass.java
@@ -1,26 +1,10 @@
-import javax.swing.*;
-import java.util.Random;
-
-public class DogClass {
-    public String name;
-    public int x, y;
-    public ImageIcon icon;
-
-    // Updated constructor to accept both name and iconPath
+public class DogClass extends AnimalClass {
     public DogClass(String name) {
-        this.name = name;
-        this.x = 0;
-        this.y = 0;
-        this.icon = new ImageIcon("resources/dog_icon.png");  // Ensure correct path
+        super(name, "resources/dog_icon.png"); 
     }
 
+    @Override
     public void makeSound() {
         System.out.println("Woof!");
-    }
-
-    public void move() {
-        // Random movement logic
-        this.x = new Random().nextInt(100);
-        this.y = new Random().nextInt(100);
     }
 }
