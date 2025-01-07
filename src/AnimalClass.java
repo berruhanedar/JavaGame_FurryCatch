f1e1284fee54995bddaae5a570487f61e0bce463
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2025-01-07 18:42:24 +0300

    Updated AnimalClass to include iconPath and sound functionality

--- src/AnimalClass.java
+++ src/AnimalClass.java
@@ -6,21 +6,23 @@ public class AnimalClass {
     protected int x, y;
     protected ImageIcon icon;
 
-    public AnimalClass(String name) {
+    public AnimalClass(String name, String iconPath) {
         this.name = name;
-        this.x = 50; // Starting position (middle point)
+        this.x = 50; 
         this.y = 50;
+        this.icon = new ImageIcon(iconPath);
     }
 
     public void move() {
-        this.x = new Random().nextInt(101);
-        this.y = new Random().nextInt(101);
+        this.x = new Random().nextInt(101); 
+        this.y = new Random().nextInt(101); 
     }
 
     public String getPosition() {
         return "(" + x + ", " + y + ")";
     }
 
-   public void makeSound() {
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
     }
 }
